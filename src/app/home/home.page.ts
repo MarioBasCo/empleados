1f7a59bea2002f8515f40f9d330f5376b9c06ebc
MarioBasCo <mario-basilio1998@hotmail.com>
2022-04-11 13:34:40 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { ServicioService } from './../services/servicio.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,80 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  id = '';
+  empleado = {
+    nombre: '',
+    cargo: '',
+    salario: 0,
+  }
 
-  constructor() {}
+  listaDeUsuarios: any[] = [];
+
+  constructor(private database: ServicioService) { }
+
+  ngOnInit() {
+    this.database.getAll('empleados').then(firebaseResponse => {
+      firebaseResponse.subscribe(listaDeUsuariosRef => {
+
+        this.listaDeUsuarios = listaDeUsuariosRef.map(usuarioRef => {
+          let usuario = usuarioRef.payload.doc.data();
+          usuario['id'] = usuarioRef.payload.doc.id;
+          return usuario;
+        })
+        console.log(this.listaDeUsuarios);
+
+      })
+    })
+  }
+
+  eliminar(id) {
+    this.database.delete('empleados', id).then(res => {
+      alert("Se elimino con exito");
+    }).catch(err => {
+      console.log("ERROR al eliminar ", err);
+    });
+  }
+
+
+  altaUsuario() {
+    if(this.id.length > 0) {
+      this.database.update('empleados', this.id, this.empleado).then(res => {
+        this.id = '';
+        this.empleado.nombre = '';
+        this.empleado.cargo = '';
+        this.empleado.salario = 0;
+        alert("se modifico el usuario");
+      }).catch(err => {
+        console.log("Error al modificar: ", err)
+      });
+    } else {
+      this.database.create('empleados', this.empleado).then(res => {
+        console.log(res);
+        this.id = '';
+        this.empleado.nombre = '';
+        this.empleado.cargo = '';
+        this.empleado.salario = 0;
+      }).catch(err => {
+        console.log("error en alta: ", err);
+      });
+    }
+  }
+
+  modificar(item) {
+    this.id = item.id;
+    this.empleado.nombre = item.nombre;
+    this.empleado.cargo = item.cargo;
+    this.empleado.salario = item.salario;
+  }
+
+  obtenerPorId(id) {
+    this.database.getById('usuarios', id).then(res => {
+      res.subscribe(docRef => {
+        let usuario = docRef.data();
+        usuario['id'] = docRef.id;
+        console.log(usuario)
+      })
+    })
+  }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'empleados-48fa1',
+    appId: '1:1001767153131:web:d01b4804afdb852860fdae',
+    storageBucket: 'empleados-48fa1.appspot.com',
+    apiKey: 'AIzaSyD2fr-7DngHkKubDiVT5v8MT4zB7RcxgCs',
+    authDomain: 'empleados-48fa1.firebaseapp.com',
+    messagingSenderId: '1001767153131',
+  },
   production: true
 };
